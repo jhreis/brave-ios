1c8c65f79318676d22d7dc245eb9ecc981f8be79
Brandon <JustBrandonT@gmail.com>
2019-07-08 12:22:22 -0400

    Fixed punycoding of URLs and emails. This fixes the ability to search with quoted queries. This fixes the ability to search with email address as a search query. Fixes https://github.com/brave/brave-ios/issues/1209

--- Client/Frontend/Browser/URIFixup.swift
+++ Client/Frontend/Browser/URIFixup.swift
@@ -32,6 +32,18 @@ class URIFixup {
         if trimmed.range(of: " ") != nil {
             return nil
         }
+        
+        // A URL is only valid when the URL has a scheme, is not an email, is not quoted, and can be punycoded.
+        // If one of the above conditions is NOT satisfied, the URL is invalid and should be deemed "search terms" search terms instead. Technically, an email is also a valid URL but does not get handled by the DNS server.
+        // I'm not sure if the punycoded prior to my if-statement below is correct - Brandon T.
+        //
+        // According to RFC 6531, Page 6, SMTP Extension for SMTPUTF8, UTF-8 emails are allowed.
+        // However, according to RFC 3492 - IDNA Punycode, only the DOMAIN is supposed to be punycoded and not the entire URL. The rest of the URL is URLEncoded (IE: The path, the query, etc..)
+        // The below if-statement fixes it by validating email as well as punycoding the "non-escaped" URL.
+        // The below if-statement allows us to search emails and quoted strings - brave-ios/issues/1209.
+        if punycodedURL(trimmed) == nil || isValidEmail(trimmed) {
+            return nil
+        }
 
         // If there is a ".", prepend "http://" and try again. Since this
         // is strictly an "http://" URL, we also require a host.
@@ -49,5 +61,17 @@ class URIFixup {
             components?.host = host
         }
         return components?.url
+    }
+    
+    static func isValidEmail(_ string: String) -> Bool {
+        let regex =
+            "(?:[a-zA-Z0-9!#$%\\&‘*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}" +
+            "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+            "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
+            "z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5" +
+            "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+            "9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+            "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        return NSPredicate(format: "SELF MATCHES[c] %@", regex).evaluate(with: string)
     }
 }
